15e5c1e1fd516153faf62f2a1cae8bcb964a5d16
knizinsky <108873272+knizinsky@users.noreply.github.com>
2023-01-08 20:42:34 +0100

    xchg button added with functionality

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let dx = document.getElementById('dx');
 
 const randomBtn = document.getElementById('randomBtn');
 const moveBtn = document.getElementById('moveBtn');
+const xchgBtn = document.getElementById('xchgBtn');
 const axRadioFirst = document.getElementById('axRadioFirst');
 const bxRadioFirst = document.getElementById('bxRadioFirst');
 const cxRadioFirst = document.getElementById('cxRadioFirst');
@@ -16,12 +17,65 @@ const dxRadioSecond = document.getElementById('dxRadioSecond');
 
 randomBtn.addEventListener('click', randomHex);
 moveBtn.addEventListener('click', moveValue);
+xchgBtn.addEventListener('click', xchgValue);
+
+function xchgValue() {
+	if (axRadioFirst.checked && bxRadioSecond.checked) {
+		let temp = bx.value;
+		bx.value = ax.value;
+		ax.value = temp;
+	} else if (axRadioFirst.checked && cxRadioSecond.checked) {
+		let temp = cx.value;
+		cx.value = ax.value;
+		ax.value = temp;
+	} else if (axRadioFirst.checked && dxRadioSecond.checked) {
+		let temp = dx.value;
+		dx.value = ax.value;
+		ax.value = temp;
+	} else if (bxRadioFirst.checked && axRadioSecond.checked) {
+		let temp = ax.value;
+		ax.value = bx.value;
+		bx.value = temp;
+	} else if (bxRadioFirst.checked && cxRadioSecond.checked) {
+		let temp = cx.value;
+		cx.value = bx.value;
+		bx.value = temp;
+	} else if (bxRadioFirst.checked && dxRadioSecond.checked) {
+		let temp = dx.value;
+		dx.value = bx.value;
+		bx.value = temp;
+	} else if (cxRadioFirst.checked && axRadioSecond.checked) {
+		let temp = ax.value;
+		ax.value = cx.value;
+		cx.value = temp;
+	} else if (cxRadioFirst.checked && bxRadioSecond.checked) {
+		let temp = bx.value;
+		bx.value = cx.value;
+		cx.value = temp;
+	} else if (cxRadioFirst.checked && dxRadioSecond.checked) {
+		let temp = dx.value;
+		dx.value = cx.value;
+		cx.value = temp;
+	} else if (dxRadioFirst.checked && axRadioSecond.checked) {
+		let temp = ax.value;
+		ax.value = dx.value;
+		dx.value = temp;
+	} else if (dxRadioFirst.checked && bxRadioSecond.checked) {
+		let temp = bx.value;
+		bx.value = dx.value;
+		dx.value = temp;
+	} else if (dxRadioFirst.checked && cxRadioSecond.checked) {
+		let temp = cx.value;
+		cx.value = dx.value;
+		dx.value = temp;
+	}
+}
 
 function moveValue() {
-	ax.value;
-	bx.value;
-	cx.value;
-	dx.value;
+	// ax.value;
+	// bx.value;
+	// cx.value;
+	// dx.value;
 
 	if (axRadioFirst.checked && bxRadioSecond.checked) {
 		bx.value = ax.value;
@@ -29,6 +83,24 @@ function moveValue() {
 		cx.value = ax.value;
 	} else if (axRadioFirst.checked && dxRadioSecond.checked) {
 		dx.value = ax.value;
+	} else if (bxRadioFirst.checked && axRadioSecond.checked) {
+		ax.value = bx.value;
+	} else if (bxRadioFirst.checked && cxRadioSecond.checked) {
+		cx.value = bx.value;
+	} else if (bxRadioFirst.checked && dxRadioSecond.checked) {
+		dx.value = bx.value;
+	} else if (cxRadioFirst.checked && axRadioSecond.checked) {
+		ax.value = cx.value;
+	} else if (cxRadioFirst.checked && bxRadioSecond.checked) {
+		bx.value = cx.value;
+	} else if (cxRadioFirst.checked && dxRadioSecond.checked) {
+		dx.value = cx.value;
+	} else if (dxRadioFirst.checked && axRadioSecond.checked) {
+		ax.value = dx.value;
+	} else if (dxRadioFirst.checked && bxRadioSecond.checked) {
+		bx.value = dx.value;
+	} else if (dxRadioFirst.checked && cxRadioSecond.checked) {
+		cx.value = dx.value;
 	}
 }
 
